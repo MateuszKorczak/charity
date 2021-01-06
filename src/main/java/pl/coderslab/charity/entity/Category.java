cabb567dcb136744d637562282f4e3405de6ed3d
Mateusz <mateusz91korczak@gmail.com>
2021-01-06 13:11:09 +0100

    Add Category's entity and repository.

---
+++ src/main/java/pl/coderslab/charity/entity/Category.java
@@ -0,0 +1,39 @@
+package pl.coderslab.charity.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    @Size(min = 3, max = 20)
+    private String name;
+
+
+
+/* getters and setters */
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/webapp/WEB-INF/views/footer.jsp
+++ src/main/webapp/WEB-INF/views/footer.jsp
@@ -18,8 +18,8 @@
     <div class="bottom-line">
         <span class="bottom-line--copy">Copyright &copy; 2018</span>
         <div class="bottom-line--icons">
-            <a href="#" class="btn btn--small"><img src="<c:url value="resources/images/icon-facebook.svg"/>"/></a>
-            <a href="#" class="btn btn--small"> <img src="<c:url value="resources/images/icon-instagram.svg"/>"/></a>
+            <a href="#" class="btn btn--small"><img src="<c:url value="resources/images/icon-facebook.svg"/>" alt="facebook"/></a>
+            <a href="#" class="btn btn--small"> <img src="<c:url value="resources/images/icon-instagram.svg"/>" alt="instagram"/></a>
         </div>
     </div>
 </footer>
